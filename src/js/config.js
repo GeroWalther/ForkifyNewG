4b6e7d69a56ee19c78e117c10377f84ecef0771b
GeroWalther <gero.walther@gmail.com>
2022-06-08 18:33:00 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9a396975-bed5-429f-a5ee-977936415cca';
+export const MODAL_CLOSE_SEC = 2.5;
